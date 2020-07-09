37b372aeec8bfabf84603911b91c6fb562224246
Anabella Spinelli <anabellaspinelli@gmail.com>
2020-07-09 11:23:39 +0200

    turrinator exploration

--- src/app.js
+++ src/app.js
@@ -2,12 +2,17 @@ const express = require('express')
 const app = express()
 
 const { handleParcel } = require('./handlers')
+const { handleTurrinator, handleInteractive } = require('./turri-handlers')
 
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.send('<h1>Parcel</h1>'))
 app.get('/_status', (req, res) => res.send('Up and running!!'))
+// app.get('/_status', (req, res) => res.send('Up and running!!'))
 
-app.post('/parcel', handleParcel)
+// app.post('/parcel', handleParcel)
+
+// app.post('/turrinator', handleTurrinator)
+// app.post('/interactive', handleInteractive)
 
 module.exports = app
